6d769ee1eb4c4325ea267fea2ddba46b5b9c1003
karamala ganesh <gkaramala@dstworldwideservices.com>
2015-06-29 21:52:54 +0530

    Heruko port configuration

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ module.exports = exports = function(app,db) {
 
 var contentHandler = new ContentHandler(db);
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
  app.get("/getData/cinema/:name", contentHandler.getCinema);
 
 }
