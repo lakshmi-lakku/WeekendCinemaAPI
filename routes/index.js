c0fb0f50526bc461dd373ebcda2a216fec091b7e
Ganesh-Karamala <ganeshkaramala@gmail.com>
2015-11-17 14:45:29 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,21 @@ var CinemaContentHandler = require('./CinemaContentHandler');
 
 var EventContentHandler = require('./EventContentHandler');
 
-module.exports = exports = function(app,db) {
+module.exports = exports = function(app, db) {
 
-var cinemaContentHandler = new CinemaContentHandler(db);
+    var cinemaContentHandler = new CinemaContentHandler(db);
 
-var eventContentHandler = new EventContentHandler(db);
+    var eventContentHandler = new EventContentHandler(db);
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+    app.get("/v1/cinema/:name", cinemaContentHandler.getCinema);
 
- app.get("/v1/cinema/:name", cinemaContentHandler.getCinema);
+    app.get("/v1/events/:date", eventContentHandler.getEventsByDate);
 
- app.get("/v1/events/:date",eventContentHandler.getEventsByDate);
- 
- app.get("/v1/events/",eventContentHandler.getEvents);
+    app.get("/v1/events/", eventContentHandler.getEvents);
 }
